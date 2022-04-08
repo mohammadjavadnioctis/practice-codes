54eeab641ef5720a29c7cc02cd0e3c1b5d85cf9d
M.J <46086102+mohammadjavadnioctis@users.noreply.github.com>
2022-04-08 17:33:35 +0300

    Update practice.ts

--- practice.ts
+++ practice.ts
@@ -7,3 +7,18 @@ let myNumArr = getArray<number>([100, 200, 300]);
 let myStrArr = getArray<string>(["Hello", "World"]);
 myNumArr.push(1)
 console.log(myNumArr)
+
+
+const foo = <T,>(x: T) => x;
+
+//forwarding refs in typescript
+
+
+const Component = React.forwardRef<RefType, PropsType>((props, ref) => {
+  return someComponent;
+});
+
+const Search = React.forwardRef<HTMLInputElement>((props, ref) => {
+  return <input ref={ref} type="search" />;
+});
+
